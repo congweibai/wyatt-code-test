d41ab7a35fd7d6be767efc5dab83e83511f6f4dd
Wyatt Bai <53634020+congweibai@users.noreply.github.com>
2024-08-15 19:48:31 +0930

    feat: useMovieDetail hook

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -1,7 +1,29 @@
 import { useSelectedMovie } from "@/contexts/selectedMovieContext/useSelectedMovie";
+import { useMovieDetail } from "@/pages/movie/hooks";
+import { Box, CircularProgress } from "@mui/material";
+import { useEffect } from "react";
 
 export const MovieDetails = () => {
   const { selectedImdbID } = useSelectedMovie();
 
-  return <>Detail goes here: {selectedImdbID}</>;
+  const { loading, response, getMovieDetail } = useMovieDetail();
+
+  useEffect(() => {
+    if (selectedImdbID) getMovieDetail({ imdbID: selectedImdbID });
+  }, [selectedImdbID]);
+
+  if (loading)
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    );
+
+  if (!selectedImdbID) return <>Please select movie to view detail</>;
+
+  return (
+    <>
+      Detail goes here: {selectedImdbID}, name: {response?.Title}
+    </>
+  );
 };
--- src/pages/movie/hooks/index.ts
+++ src/pages/movie/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from "./useMovieSearch";
+export * from "./useMovieDetail";
